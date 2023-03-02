766c156244cf477a73332fc27530e8787913743e
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-02 16:35:36 -0400

    button

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [items, setItems] = useState([]);
+
+  function onRemoveItem(itemToRemove) {
+    const newItems = items.filter((item) => {
+      return item !== itemToRemove;
+    });
+    setItems(newItems);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const input = form.item;
+    const newItems = [...items, input.value];
+    setItems(newItems);
+    form.reset();
+  }
 
   return (
     <div>
       <h1>Shopping List</h1>
       <div className="shopping-list">
         <h2>Items to buy</h2>
-        <form onSubmit={undefined}>
-          <input
-            type="text"
-            name="item"
-            placeholder="Add a new item"
-            required
-          />
-          <button>Add</button>
+        <form onSubmit={onSubmit}>
+          <div className="input-container">
+            <input
+              type="text"
+              name="item"
+              placeholder="Add a new item"
+              required
+            />
+            {/* <button>Add</button> */}
+            <button className="cta">
+              <span>Hover me</span>
+              <svg viewBox="0 0 13 10" height="10px" width="15px">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+              </svg>
+            </button>
+          </div>
         </form>
         <ul>
           {items.map((item, index) => (
-            <Item />
+            <Item key={item + index} onRemoveItem={onRemoveItem} item={item} />
           ))}
         </ul>
       </div>
@@ -29,7 +54,14 @@ function App() {
 }
 
 function Item({ item, onRemoveItem }) {
-  return <li></li>;
+  return (
+    <li>
+      - {item}
+      <button className="delete" onClick={() => onRemoveItem(item)}>
+        x
+      </button>
+    </li>
+  );
 }
 
 export default App;
