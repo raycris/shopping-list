72449dcc831982a3ddcac370071d73ae6f46efb1
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-02 16:55:08 -0400

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
             />
             {/* <button>Add</button> */}
             <button className="cta">
-              <span>Hover me</span>
+              <span>Add me</span>
               <svg viewBox="0 0 13 10" height="10px" width="15px">
                 <path d="M1,5 L11,5"></path>
                 <polyline points="8 1 12 5 8 9"></polyline>
